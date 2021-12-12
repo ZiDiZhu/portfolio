b1f9e752b2e8eae61d136aa76d489fdb0edfe380
Nebularswamp <grosswamp@gmail.com>
2021-12-11 20:39:40 -0500

    click to open window basic action added

--- scripts/games.js
+++ scripts/games.js
@@ -1,9 +1,10 @@
-var game = {
+var pluralPlatformers = {
   title: "plural platformers",
   year: "2020",
   tool: "p5.js"
 }
 
-function openWindow(){
-  console.log("aaaa");
+function openWindow(myGame){
+  console.log(myGame.title);
+  $('#infoWindow').css("display","inline-block");
 }
