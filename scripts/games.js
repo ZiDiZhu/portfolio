21c0dffa8a5eb4f462cfc4f6e257b16202fcdb41
Nebularswamp <grosswamp@gmail.com>
2021-12-11 21:50:51 -0500

    made infowindow template!

--- scripts/games.js
+++ scripts/games.js
@@ -1,10 +1,25 @@
 var pluralPlatformers = {
   title: "plural platformers",
   year: "2020",
+  genre: "puzzle / platformer",
+  bgColor:"hotpink",
   tool: "p5.js"
 }
 
 function openWindow(myGame){
   console.log(myGame.title);
-  $('#infoWindow').css("display","inline-block");
+  showThing('#infoWindow');
+  $('#title').text(myGame.title);
+  $('#year').text(myGame.year);
+  $('#genre').text(myGame.genre);
+  $('#infoWindow').css("background-color",myGame.bgColor);
+}
+
+//currently only used for game info window, but probably reusable.
+function showThing(thing){
+  $(thing).css("display","inline-block");
+}
+
+function hideThing(thing){
+  $(thing).css("display","none");
 }
