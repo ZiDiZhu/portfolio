140c918390f6eb4adadeec9129146b48a7c3361f
Nebularswamp <grosswamp@gmail.com>
2021-12-13 15:17:29 -0500

    added vcmc/cartpop212 projects info

--- scripts/multimediainfo.js
+++ scripts/multimediainfo.js
@@ -1,12 +1,25 @@
 var vcmc = {
   title: "VCMC",
   year: "2021",
-  link: "https://zidizhu.github.io/cart263/projects/project2-fruitjam-prototype/",
+  link: "https://www.youtube.com/watch?v=qmHF3_Qzb8c&t=16s",
   genre: "music toy",
-  tool: "p5.js",
-  description:`Jam session with the fruit band!`,
-  controls:`click on buttons!`,
-  bgColor:"purple",
-  icon:"logos/logo_FJ_sp.png",
+  tool: "Arduino",
+  description:`DIY Beat machine!`,
+  controls:`click on buttons and turn knobs!`,
+  bgColor:"blue",
+  icon:"logos/logo_vcmc_sp.png",
   screenshot:"https://www.youtube.com/embed/r0qeZxGG1_Y"
+};
+
+var popcart212 = {
+  title: "POPCART212",
+  year: "2021",
+  link: "https://www.youtube.com/watch?v=5aKWHU6oRkU",
+  genre: "music video",
+  tool: "Ableton, Sony Vegas",
+  description:`ORIGINAL POP SONG`,
+  controls:`N/A`,
+  bgColor:"orange",
+  icon:"logos/logo_cartpop212_sp.png",
+  screenshot:"https://www.youtube.com/embed/5aKWHU6oRkU"
 };
