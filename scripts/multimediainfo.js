951811880400daab8768d0141d5bc1b6ecd7ddf8
Nebularswamp <grosswamp@gmail.com>
2021-12-13 14:58:21 -0500

    ADDED: multimedia.html
    using same template infowindow! screenshot link works with video but CAUSION: MAKE SURE THE LINK HAS /embed

---
+++ scripts/multimediainfo.js
@@ -0,0 +1,12 @@
+var vcmc = {
+  title: "VCMC",
+  year: "2021",
+  link: "https://zidizhu.github.io/cart263/projects/project2-fruitjam-prototype/",
+  genre: "music toy",
+  tool: "p5.js",
+  description:`Jam session with the fruit band!`,
+  controls:`click on buttons!`,
+  bgColor:"purple",
+  icon:"logos/logo_FJ_sp.png",
+  screenshot:"https://www.youtube.com/embed/r0qeZxGG1_Y"
+};
