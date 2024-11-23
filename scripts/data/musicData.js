478e479d60478dffd698cc4ea4dcb313bdef7b05
Zi Di <grosswamp@gmail.com>
2024-11-23 17:23:43 -0500

    music page cleaner relayout

---
+++ scripts/data/musicData.js
@@ -0,0 +1,17 @@
+const vol1 = {
+    title: "⊹☾⋆⁺₊. ݁₊ ⊹Vol.1 . ݁˖ . ݁⋆.˚✮",
+    cover:"art/gifs/home.gif",
+    songs: [
+        { name: "anemoia", src: "audio/anemoia.wav"},
+        { name: "seeyouinmydream", src: "audio/seeyouinmydream.wav"}
+    ]
+};
+
+const vol2 = {
+    title: "⊹☾⋆⁺₊. ݁₊ ⊹Vol.2 . ݁˖ . ݁⋆.˚✮",
+    cover:"art/gifs/yip-music.gif",
+    songs: [
+        { name: "catland", src: "audio/bgm_catland.wav"},
+        { name: "wormhole", src: "audio/wormhole.wav"}
+    ]
+}
